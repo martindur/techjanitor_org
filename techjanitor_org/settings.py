3aa063a68ec139513e9b043d05e37705931c98a9
Martin Durhuus <martindurhuus@gmail.com>
2020-01-16 16:10:12 +0100

    Updates allowed hosts

--- techjanitor_org/settings.py
+++ techjanitor_org/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'techjanitor.org']
 
 if DEBUG:
     SECRET_KEY = 'dev'
